8a345c458cb9ac31a0a1764455ea5ca6afabe76a
Ryan Scott <ryankennethscott@gmail.com>
2019-07-06 16:13:40 +1200

    First version

---
+++ go.mod
@@ -0,0 +1,16 @@
+module go_gym_backend
+
+go 1.12
+
+require (
+	github.com/asdine/storm v2.1.2+incompatible
+	github.com/davecgh/go-spew v1.1.1
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/mux v1.7.2
+	github.com/mattn/go-sqlite3 v1.10.0
+	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/tidwall/gjson v1.2.1
+	github.com/tidwall/match v1.0.1 // indirect
+	github.com/tidwall/pretty v1.0.0 // indirect
+	go.etcd.io/bbolt v1.3.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,465 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/asdine/storm"
+	"github.com/asdine/storm/q"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/tidwall/gjson"
+)
+
+var allClubs = map[string]Club{
+	"01": Club{"01", "Auckland City"},
+	"09": Club{"09", "Britomart"},
+	"13": Club{"13", "Newmarket"},
+	"06": Club{"06", "Takapuna"},
+}
+var db *storm.DB
+
+type Club struct {
+	ClubCode string `json:"ClubCode" storm:"index"`
+	Name     string `json:"Name"`
+}
+
+type Query struct {
+	name []string    `json:"name"`
+	club []Club      `json:"club"`
+	date []time.Time `json:"date"`
+	hour []int       `json:"hour"`
+}
+
+type Instructor struct {
+	Description        string `json:"Description"`
+	InstructorID       string `json:"InstructorId"`
+	InstructorMemberID string `json:"InstructorMemberId"`
+	Name               string `json:"Name"`
+}
+
+type Class struct {
+	ClassCode           string     `json:"ClassCode"`
+	ClassDefinitionID   string     `json:"ClassDefinitionId"`
+	ClassDescription    string     `json:"ClassDescription"`
+	ClassInstanceID     string     `storm:"id" json:"ClassInstanceId"`
+	ClassName           string     `storm:"index" json:"ClassName"`
+	Club                Club       `storm:"index" json:"Club"`
+	Duration            int        `storm:"index" json:"Duration"`
+	StartDay            int        `storm:"index" json:-`
+	StartHour           int        `storm:"index" json:-`
+	StartDateTime       time.Time  `storm:"index" json:"StartDateTime"`
+	EndDateTime         time.Time  `json:"EndDateTime"`
+	Equipment           string     `json:"Equipment"`
+	ExerciseType        string     `json:"ExerciseType"`
+	Intensity           string     `json:"Intensity"`
+	IsVirtualClass      bool       `json:"IsVirtualClass"`
+	MainInstructor      Instructor `json:"MainInstructor"`
+	SecondaryInstructor Instructor `json:"SecondaryInstructor"`
+	Site                struct {
+		Capacity int    `json:"Capacity"`
+		SiteID   string `json:"SiteId"`
+		SiteName string `json:"SiteName"`
+	} `json:"Site"`
+	Status string `json:"Status"`
+}
+type ClassAlias Class
+
+func (c Class) MarshalJSON() ([]byte, error) {
+	return json.Marshal(NewJSONClass(c))
+}
+func (c *Class) UnmarshalJSON(data []byte) error {
+	var cd JSONClass
+	if err := json.Unmarshal(data, &cd); err != nil {
+		return err
+	}
+	*c = cd.Class()
+	return nil
+}
+
+func NewJSONClass(class Class) JSONClass {
+	return JSONClass{
+		ClassAlias(class),
+		class.StartDateTime.Day(),
+		class.StartDateTime.Hour(),
+	}
+}
+
+type JSONClass struct {
+	ClassAlias
+	StartDay  int `json:"StartDay"`
+	StartHour int `json:"StartHour"`
+}
+
+func (cd JSONClass) Class() Class {
+	class := Class(cd.ClassAlias)
+	class.StartDay = cd.StartDay
+	class.StartHour = cd.StartHour
+	return class
+}
+
+type ClassType struct {
+	ID    string
+	Key   string `json:"Key"`
+	Value string `json:"Value"`
+}
+
+func ClassesHandler(w http.ResponseWriter, r *http.Request) {
+
+	/* name="BodyPump, RPM"&club="Auckland City"&date="2018-07-18,2018-07-19"&hour=11 */
+	// Get query parameters
+	params := r.URL.Query()
+	var q = Query{}
+
+	// Parse names
+	n := params.Get("name")
+	if n != "" {
+		ns := strings.Split(string(n), ",")
+		q.name = ns
+	}
+
+	// Parse dates
+	d := params.Get("date")
+	if d != "" {
+		ds := strings.Split(string(d), ",")
+		var dates []time.Time
+		for _, v := range ds {
+			t, err := time.Parse("2006-01-02", v)
+			if err != nil {
+				fmt.Printf("Failed to parse date string - %s \n", err)
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("Failed to parse date parameter"))
+				return
+			}
+			dates = append(dates, t)
+		}
+		q.date = dates
+	}
+
+	// Parse clubs
+	c := params.Get("club")
+	if c != "" {
+		cs := strings.Split(string(c), ",")
+		var clubs []Club
+		for _, v := range cs {
+			var club Club
+			club = allClubs[v]
+			clubs = append(clubs, club)
+		}
+		q.club = clubs
+	}
+
+	// Parse hours
+	h := params.Get("hour")
+	if h != "" {
+		hs := strings.Split(string(h), ",")
+		var hours []int
+		for _, v := range hs {
+			hrs, err := strconv.Atoi(v)
+			if err != nil {
+				fmt.Printf("Failed to parse hours string - %s \n", err)
+				// TODO return 400
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("Failed to parse hours parameter"))
+				return
+			}
+			hours = append(hours, hrs)
+
+		}
+		q.hour = hours
+	}
+	classes, err := queryClasses(db, q)
+	if err != nil {
+		fmt.Printf("Failed to query classes - %s \n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Failed to return classes"))
+		return
+	}
+
+	jClasses, err := json.Marshal(classes)
+	if err != nil {
+		fmt.Printf("Failed to marshal classes to JSON - %s \n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Failed to return classes"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(jClasses))
+}
+
+func ClassTypesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gorilla!\n"))
+}
+
+func AnalyticsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gorilla!\n"))
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gorilla!\n"))
+}
+
+func getClasses() ([]Class, []ClassType, error) {
+
+	log.Printf("Fetching classes\n")
+	resp, err := http.Post("https://www.lesmills.co.nz/api/timetable/get-timetable-epi", "application/x-www-form-urlencoded", strings.NewReader("Club=01,09,13,06"))
+
+	if err != nil {
+		log.Printf("Failed to retrieve classes from Les Mills - %s \n", err)
+		return nil, nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Returned a non-OK response code (%d) from Les Mills\n", resp.StatusCode)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Failed to read response body from Les Mills - %s \n", err)
+	}
+	jsonResponse := (string(body))
+	cs := gjson.Get(jsonResponse, "Classes")
+	cts := gjson.Get(jsonResponse, "ClassType")
+
+	// Get Classes
+	var classes []Class
+	err = json.Unmarshal([]byte(cs.String()), &classes)
+	if err != nil {
+		// TODO stuff
+		log.Printf("Failed to unmarshal classes %s\n", err)
+		return nil, nil, err
+	}
+	log.Printf("Fetched %d classes\n", len(classes))
+
+	// Get ClassTypes
+	var classTypes []ClassType
+	err = json.Unmarshal([]byte(cts.String()), &classTypes)
+	if err != nil {
+		log.Printf("Failed to unmarshal classtypes %s\n", err)
+		return nil, nil, err
+	}
+	log.Printf("Fetched %d class types\n", len(classTypes))
+	return classes, classTypes, nil
+}
+
+func saveClasses(db *storm.DB, classes []Class) error {
+	log.Printf("Saving %d classes\n", len(classes))
+	t1 := time.Now()
+
+	for _, c := range classes {
+		err := db.Save(&c)
+		if err != nil {
+			log.Printf("Failed to save classes - %s\n", err)
+			return err
+		}
+	}
+	t2 := time.Now()
+	diff := t2.Sub(t1)
+	log.Printf("Classes saved in %s\n", diff)
+	return nil
+}
+
+func saveClassTypes(db *storm.DB, classTypes []ClassType) error {
+	log.Printf("Saving %d classtypes\n", len(classTypes))
+	t1 := time.Now()
+	for _, c := range classTypes {
+		c.ID = uuid.New().String() // This is for storm
+		err := db.Save(&c)
+		if err != nil {
+			log.Printf("Failed to save class types - %s\n", err)
+			return err
+		}
+	}
+	t2 := time.Now()
+	diff := t2.Sub(t1)
+	log.Printf("Classtypes saved in %s\n", diff)
+	return nil
+
+}
+
+// TODO: This logic is a bit silly
+func createMatcher(m ...[]q.Matcher) q.Matcher {
+	// Create a bitmap of the matchers to see if they're enabled
+	queries := 0
+	for k, v := range m {
+		if len(v) > 0 {
+			queries |= (1 << uint(k))
+		}
+	}
+	// Based on the bitmap, add them to the matcher list
+	switch queries {
+	case 0:
+		return nil
+	case 1: // (0001)
+		return q.And(q.Or(m[0]...))
+	case 2: //(0010)
+		return q.And(q.Or(m[1]...))
+	case 3: //(0011)
+		return q.And(q.Or(m[1]...), q.Or(m[0]...))
+	case 4: //(0100)
+		return q.And(q.Or(m[2]...))
+	case 5: //(0101)
+		return q.And(q.Or(m[2]...), q.Or(m[0]...))
+	case 6: //(0110)
+		return q.And(q.Or(m[2]...), q.Or(m[1]...))
+	case 7: //(0111)
+		return q.And(q.Or(m[2]...), q.Or(m[1]...), q.Or(m[0]...))
+	case 8: //(1000)
+		return q.And(q.Or(m[3]...))
+	case 9: //(1001)
+		return q.And(q.Or(m[3]...), q.Or(m[0]...))
+	case 10: //(1010)
+		return q.And(q.Or(m[3]...), q.Or(m[1]...))
+	case 11: //(1011)
+		return q.And(q.Or(m[3]...), q.Or(m[1]...), q.Or(m[0]...))
+	case 12: //(1100)
+		return q.And(q.Or(m[3]...), q.Or(m[2]...))
+	case 13: //(1101)
+		return q.And(q.Or(m[3]...), q.Or(m[2]...), q.Or(m[0]...))
+	case 14: //(1110)
+		return q.And(q.Or(m[3]...), q.Or(m[2]...), q.Or(m[1]...))
+	case 15: //(1111)
+		return q.And(q.Or(m[3]...), q.Or(m[2]...), q.Or(m[1]...), q.Or(m[0]...))
+	}
+	return nil
+
+}
+
+func queryClasses(db *storm.DB, query Query) ([]Class, error) {
+	var classes []Class
+	// Extract the names
+	var nameQueries []q.Matcher
+	log.Printf("Querying for classes using %s as name parameters\n", query.name)
+	for _, v := range query.name {
+		nameQueries = append(nameQueries, q.Eq("ClassCode", v))
+	}
+
+	// Extract the clubs
+	var clubQueries []q.Matcher
+	log.Printf("Querying for classes using %s as club parameters\n", query.club)
+	for _, v := range query.club {
+		clubQueries = append(clubQueries, q.Eq("Club", v))
+	}
+
+	// Extract the dates
+	var dateQueries []q.Matcher
+	log.Printf("Querying for classes using %s as date parameters\n", query.date)
+	for _, v := range query.date {
+		endOfDay := time.Date(v.Year(), v.Month(), v.Day(), 23, 59, 59, 0, v.Location())
+		startOfDay := time.Date(v.Year(), v.Month(), v.Day(), 0, 0, 0, 0, v.Location())
+		dateQueries = append(dateQueries, q.And(q.Gt("StartDateTime", startOfDay), q.Lt("StartDateTime", endOfDay)))
+	}
+
+	// Extract the hours
+	var hourQueries []q.Matcher
+	log.Printf("Querying for hours using %d as hour parameters\n", query.hour)
+	for _, v := range query.hour {
+		hourQueries = append(hourQueries, q.Eq("StartHour", v))
+	}
+
+	// Combine the matchers
+	matcher := createMatcher(nameQueries, clubQueries, dateQueries, hourQueries)
+	var err error
+	// If we don't have query parameters
+	if matcher == nil {
+		log.Printf("We have no matchers so returning all classes\n")
+		err = db.All(&classes)
+
+	} else {
+		err = db.Select(matcher).Find(&classes)
+	}
+	if err != nil {
+		log.Printf("Failed to select classes - %s\n", err)
+		return nil, err
+	}
+
+	log.Printf("Returning %d classes\n", len(classes))
+	// for _, v := range classes {
+	// 	fmt.Println("~~~~~")
+	// 	fmt.Println(v.StartDateTime)
+	// 	fmt.Println(v.ClassName)
+	// 	fmt.Println(v.Club)
+	// 	fmt.Println("~~~~~")
+	// }
+	return classes, nil
+}
+
+func main() {
+	// Create the DB
+	var err error
+	db, err = storm.Open("classes.db")
+	if err != nil {
+		log.Fatalf("Failed to open database with error  - %s \n", err)
+	}
+
+	// // Drop all old data
+	// err = db.Drop("Class")
+	// log.Printf("Dropping all old class data")
+	// if err != nil {
+	// 	log.Printf("Failed to drop data - %s \n", err)
+	// }
+
+	// classes, classTypes, err := getClasses()
+	// if err != nil {
+	// 	log.Fatalf("Failed to get classes and classTypes with error - %s\n", err)
+	// }
+
+	// err = saveClasses(db, classes)
+	// if err != nil {
+	// 	log.Fatalf("Failed to save classes with error - %s\n", err)
+
+	// }
+
+	// err = saveClassTypes(db, classTypes)
+	// if err != nil {
+	// 	log.Fatalf("Failed to save class types with error - %s\n", err)
+
+	// }
+
+	//Periodically get new classes
+	// //TODO: fix me
+	// ticker := time.NewTicker(6 * time.Hour)
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case <-ticker.C:
+	// 			err = saveClasses(db, classes)
+	// 			if err != nil {
+	// 				log.Fatalf("Failed to save classes with error - %s\n", err)
+
+	// 			}
+
+	// 			err = saveClassTypes(db, classTypes)
+	// 			if err != nil {
+	// 				log.Fatalf("Failed to save class types with error - %s\n", err)
+
+	// 			}
+	// 		}
+	// 	}
+	// }()
+
+	defer db.Close()
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/classes", ClassesHandler).Methods("GET")
+	r.HandleFunc("/classtypes", ClassTypesHandler).Methods("GET")
+	r.HandleFunc("/analytics", AnalyticsHandler).Methods("POST")
+	r.HandleFunc("/health", HealthCheckHandler).Methods("GET")
+
+	// Bind to a port and pass our router in
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:3000",
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
